de5d7084faeabc439a977e233a960a398c12f749
Mariusz Wójcik <pixiproject@gmail.com>
2016-07-21 22:01:22 +0200

    some refactor but... still a lot to do

--- 1-dribble-animation/src/index.js
+++ 1-dribble-animation/src/index.js
@@ -2,67 +2,73 @@ require('./main.scss');
 
 import $ from "jquery";
 
-	let $menu = $(".menu"),
-	  $menuItems = $menu.children(".menu__item");
-	let $colorBar = $menu.children(".color-bar");
+let $menu = $(".menu"),
+    $menuItems = $menu.children(".menu__item");
+let $colorBar = $menu.children(".color-bar");
 
-	const moveClip = ($item, index) => {
-	  let itemWidth = $item.width();
-	  let itemHeight = $item.height();
+const moveClip = ($item, index) => {
+  let itemWidth = $item.width();
+  let itemHeight = $item.height();
 
-	  let clipPosition = `polygon(
-	    ${itemWidth * index}px 0, ${itemWidth * (index + 1)}px 0, 
-	    ${itemWidth * (index + 1)}px ${itemHeight}px,  ${itemWidth * index}px ${itemHeight}px
-	  )`;
-	 
-	  $colorBar.css({
-	    'shape-inside': clipPosition,
-	    '-webkit-clip-path': clipPosition
-	  })
-	}
+  let clipPosition = `polygon(
+    ${itemWidth * index}px 0, ${itemWidth * (index + 1)}px 0, 
+    ${itemWidth * (index + 1)}px ${itemHeight}px,  ${itemWidth * index}px ${itemHeight}px
+  )`;
 
-	const loadColorBar = () => {
-	  const perWidth = 100/$menuItems.length;
-	  const getColorPalette = () => {
-	    const colorArr = [];
-	    $menuItems.each((index, element) => {
-	      let $elColor = $(element).attr('hover-color');
-	      if (index == 0) {
-	         colorArr.push(`${$elColor} 0%`);
-	      }
-	      colorArr.push(`${$elColor} ${perWidth * index}%`);
-	      colorArr.push(`${$elColor} ${perWidth * (index+1)}%`);
+  $colorBar.css({
+    'shape-inside': clipPosition,
+    '-webkit-clip-path': clipPosition
+  });
+}
 
-	    })
-	    
-	    return colorArr.join(',');
-	  }
-	  $colorBar.css({'background-image': `linear-gradient(to right,  ${getColorPalette()})`});
-	  
-	  moveClip($menuItems.first(), 0)
-	}
+const loadColorBar = () => {
+  const perWidth = 100 / $menuItems.length;
+  const getColorPalette = () => {
+    const colorArr = [];
+    $menuItems.each((index, element) => {
+      let $elColor = $(element).attr('hover-color');
+      if (index == 0) {
+          colorArr.push(`${$elColor} 0%`);
+      }
+      colorArr.push(`${$elColor} ${perWidth * index}%`);
+      colorArr.push(`${$elColor} ${perWidth * (index+1)}%`);
+    });
 
-	loadColorBar();
+    return colorArr.join(',');
+  }
+  $colorBar.css({ 'background-image': `linear-gradient(to right,  ${getColorPalette()})` });
 
-	const moveBox = (index, el) => {
-	  setTimeout(() => {moveClip($(el), index)}, 1200*index); 
-	}
-	 
-	const backToStart = () => {
-	  $colorBar.css({
-	    'transition-duration': '1500ms'
-	  });
-	  moveClip($menuItems.first(), 0)
-	} 
+  moveClip($menuItems.first(), 0)
+}
 
-	const animate = () => {
-	  $colorBar.css({
-	    'transition-duration': '1000ms'
-	  });
-	  $menuItems.each(moveBox);
-	  setTimeout(() => {backToStart();}, 6500); 
-	  setTimeout(animate, 8000); 
+/*const moveBox = (index, el) => {
+    setTimeout(() => { moveClip($(el), index) }, 1200 * index);
+}
 
-	}
+const backToStart = () => {
+    $colorBar.css({
+        'transition-duration': '1500ms'
+    });
+    moveClip($menuItems.first(), 0)
+}
 
-	animate();
+const animate = () => {
+    $colorBar.css({
+        'transition-duration': '1000ms'
+    });
+    $menuItems.each(moveBox);
+    setTimeout(() => { backToStart(); }, 6500);
+    setTimeout(animate, 8000);
+
+}*/
+
+const bindMoveToClick = () => {
+	$menuItems.each(
+		(index, element) => {
+			$(element).click( () => moveClip($(element), index))
+		}
+	);
+}
+
+loadColorBar();
+bindMoveToClick();
