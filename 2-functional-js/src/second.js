6741a31a8b4355127c4566a1a4689805d1aa62ad
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2016-09-19 23:24:14 +0200

    sceond task

---
+++ 2-functional-js/src/second.js
@@ -0,0 +1,14 @@
+import {filter, reduce, map} from 'ramda';
+
+// --- find the smallest number in an array ---
+function less(a, b) {
+  return a < b ? a : b;
+}
+
+export const smallest = (list) => {
+  if (list.length) {
+    return reduce(less, list[0], list)
+  } else {
+    return null;
+  }
+}
