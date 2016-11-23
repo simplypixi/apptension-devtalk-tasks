ad62ee2e19b67d794fcf326ca961982b0b56e560
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2016-11-23 23:37:04 +0100

    ES6 - promise - clean up

--- 4-babel-webpack-es6/app/promise/layout.js
+++ 4-babel-webpack-es6/app/promise/layout.js
@@ -4,6 +4,5 @@ export default function layout(data) {
   const template = document.querySelector('#what-is-reading-template').innerHTML;
   parse(template);   // optional, speeds up future uses
   const rendered = render(template, data);
-  console.debug({template}, {rendered}, {data});
   document.querySelector('#what-is-reading').innerHTML = rendered;
 }
