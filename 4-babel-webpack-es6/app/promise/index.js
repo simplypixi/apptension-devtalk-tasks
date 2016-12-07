0d73a126b27c52cecc3b6b88b507f17440ed6f88
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2016-12-07 21:21:03 +0100

    ES6 - use multiple promises and classes

--- 4-babel-webpack-es6/app/promise/index.js
+++ 4-babel-webpack-es6/app/promise/index.js
@@ -3,40 +3,24 @@ import {sampleSize, chunk} from 'lodash';
 
 import {layout, errorLayout} from './layout';
 import {newsApiKey, readingErrorMsg} from '../config';
+import {Newspaper, MultiNewspapers} from './newspaper';
 
-function successAction({articles}) {
-  layout(chunk(sampleSize(articles, 4), 2));
+function successAction(articles) {
+  layout(chunk(sampleSize(articles, 6), 2));
   return articles;
 }
 
+function getUrl(source) {
+  //ES2015: Template Literals
+  return `https://newsapi.org/v1/articles?source=${source}&sortBy=top&apiKey=${newsApiKey}`;
+}
 
 export default function () {
-  const source = 'football-italia';
-  //ES2015: Template Literals
-  const url = `https://newsapi.org/v1/articles?source=${source}&sortBy=top&apiKey=${newsApiKey}`;
+  const urlItalia = getUrl('football-italia');
+  const urlFour = getUrl('four-four-two');
 
-  //ES2015: Promises - First class representation of a value that may be made asynchronously and be available in the future.
-  const promiseExample = new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.onload = function () {
-      const response = JSON.parse(this.response) || this.statusText;
-      if (this.status === 200) {
-        // Success
-        resolve(response);
-      } else {
-        // Something went wrong (404 etc.)
-        reject(new Error(response.message));
-      }
-    };
-    request.onerror = function () {
-      reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
-    };
-    request.open('GET', url);
-    request.send();
-  });
-
-  promiseExample
-    //ES2015: Arrow functions
+  const news = new MultiNewspapers(urlItalia, urlFour);
+  news.getArticles()
     .then((response) => successAction(response))
     .catch((response) => errorLayout({message: readingErrorMsg}));
 }
---
+++ 4-babel-webpack-es6/app/promise/newspaper.js
@@ -0,0 +1,56 @@
+//ES2015: Modules export, Class
+export class Newspaper {
+  constructor(url) {
+    this.url = url;
+  }
+
+  load(url) {
+    //ES2015: Promise
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.onload = function () {
+        const response = JSON.parse(this.response) || this.statusText;
+        if (this.status === 200) {
+          // Success
+          resolve(response);
+        } else {
+          // Something went wrong (404 etc.)
+          reject(new Error(response.message));
+        }
+      };
+      request.onerror = function () {
+        reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+      };
+      request.open('GET', url);
+      request.send();
+    });
+  }
+
+  get() {
+    return this.load(this.url)
+  }
+  getArticles() {
+    return this.get().then(({articles}) => articles)
+  }
+}
+
+export class MultiNewspapers extends Newspaper {
+  constructor(...urls) {
+    super();
+    this.urls = urls;
+  }
+
+  get() {
+    //ES2015: Multiple promises
+    return Promise.all(
+      this.urls.map((url) => super.load(url))
+    );
+  }
+
+  getArticles() {
+    return this.get().then((data) => data
+      .map(({articles}) => articles)
+        .reduce((a, b) => a.concat(b))
+    )
+  }
+}
