8e3e489670dca96730985f4dcd6e78eabe065cf2
Paweł Mikutel <mikutel.pawel@gmail.com>
2017-01-30 22:54:09 +0100

    Add html plugin and its configuration
    Plugin is used to:
    - generate html files for main and unsupported page
    - generate html files for dev and production build
    - add hashing sufix for production files

--- 5-webpack/webpack.dev.js
+++ 5-webpack/webpack.dev.js
@@ -3,10 +3,11 @@
 const path = require('path');
 const {defaultsDeep} = require('lodash');
 const baseConfig = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = defaultsDeep(baseConfig, {
   devServer: {
-    contentBase: path.join(__dirname, 'src') ,
+    contentBase: path.join(__dirname, 'src'),
     compress: true,
     port: 8080,
     hot: true,
@@ -26,5 +27,20 @@ module.exports = defaultsDeep(baseConfig, {
     alias: {
       config$: path.resolve(__dirname, 'src/env/local/conf.js')
     }
-  }
+  },
+  plugins: [
+    {},
+    new HtmlWebpackPlugin({
+      title: 'Dev main page',
+      filename: 'index.html',
+      hash: false,
+      excludeChunks: ['unsupported']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Dev unsupported page',
+      filename: 'unsupported.html',
+      hash: false,
+      excludeChunks: ['main']
+    })
+  ]
 });
--- 5-webpack/webpack.prod.js
+++ 5-webpack/webpack.prod.js
@@ -3,11 +3,27 @@
 const path = require('path');
 const {defaultsDeep} = require('lodash');
 const baseConfig = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = defaultsDeep(baseConfig, {
   resolve: {
     alias: {
       config$: path.resolve(__dirname, 'src/env/prod/conf.js')
     }
-  }
+  },
+  plugins: [
+    {},
+    new HtmlWebpackPlugin({
+      title: 'Prod main page',
+      filename: 'index.html',
+      hash: true,
+      excludeChunks: ['unsupported']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Prod unsupported page',
+      filename: 'unsupported.html',
+      hash: true,
+      excludeChunks: ['main']
+    })
+  ]
 });
