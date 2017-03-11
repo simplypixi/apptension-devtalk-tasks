29a1acf3b074ad7be811fc3d03b70bf14e9e8a63
Michal Obielecki <mobielecki@apptension.com>
2017-03-11 11:33:50 +0100

    george preparation

--- 6-html-5-api/src/components/vibration/vibration.js
+++ 6-html-5-api/src/components/vibration/vibration.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
 class Vibration extends React.Component {
 
   render() {
-    return(<div>Vibration Api</div>);
+    return(
+      <MuiThemeProvider>
+        <div className="vibration">
+          <h1>Vibration Api</h1>
+          <div className="georg-wrap">
+            <img src="../../../src/assets/images/george-clooney.jpg" alt="georg image"/>
+          </div>
+          <RaisedButton className="trigger" label="Press to play" primary={true} />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 };
 
