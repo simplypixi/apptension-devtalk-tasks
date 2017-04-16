4bfa08680c5e094dfeb6a4f077320cc419bf2923
MichalObi <ndwz123@gmail.com>
2017-04-16 18:30:00 +0200

    Add locales actions test

--- 7-testing-exercise/app/modules/locales/spec/locales.action.spec.js
+++ 7-testing-exercise/app/modules/locales/spec/locales.action.spec.js
@@ -7,11 +7,8 @@ const SET_LANGUAGE = 'SET_LANGUAGE';
 const typeObj = { SET_LANGUAGE };
 
 describe('locales action', () => {
-  it('should proper action to be defined', () => {
-    expect(localesActions).to.not.be.undefined;
-  });
-  it('should proper types to be defined', () => {
-    expect(localesActionsTypes).to.not.be.undefined;
+  it('should proper action and types to be defined', () => {
+    expect(localesActions, localesActionsTypes).to.not.be.undefined; // eslint-disable-line
   });
   it('should proper type be defined', () => {
     expect(typeObj).to.deep.equal(localesActionsTypes);
