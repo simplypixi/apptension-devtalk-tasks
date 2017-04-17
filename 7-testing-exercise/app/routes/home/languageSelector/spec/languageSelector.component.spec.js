013383f0f9de1ee42ac7dd34211a1b59aa2ecf7d
MichalObi <ndwz123@gmail.com>
2017-04-17 20:16:41 +0200

    Enzyme test for langageSelector component

--- 7-testing-exercise/app/routes/home/languageSelector/spec/languageSelector.component.spec.js
+++ 7-testing-exercise/app/routes/home/languageSelector/spec/languageSelector.component.spec.js
@@ -1,2 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
 import LanguageSelector from '../languageSelector.component';
+
+describe('<LanguageSelector />', () => {
+  const wrapper = mount(<LanguageSelector value="en" />);
+  it('should set up value as props', () => {
+    expect(wrapper.props().value).to.equal('en');
+  });
+
+  it('should allow update value via props changing', () => {
+    wrapper.setProps({ value: 'de' });
+    expect(wrapper.props().value).to.equal('de');
+  });
+});
+
+
 
