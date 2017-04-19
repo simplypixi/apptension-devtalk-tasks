df3c57b8b664636674863451de43d8b48065b9c1
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2017-04-20 00:04:16 +0200

    eslint clean up

--- 7-testing-exercise/app/routes/home/rangeSelector/spec/rangeSelector.component.spec.js
+++ 7-testing-exercise/app/routes/home/rangeSelector/spec/rangeSelector.component.spec.js
@@ -4,12 +4,15 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import { IntlProvider } from 'react-intl';
 import InputRange from 'react-input-range';
-import RangeSelector from '../RangeSelector.component';
+import RangeSelector from '../rangeSelector.component';
 
 
 describe('<AverageValue />', () => {
   it('should render one <InputRange /> components', () => {
-    const wrapper = mount(<IntlProvider locale="en"><RangeSelector rangeValues={Map({min: 0, max: 600})} setRangeValues={null} /></IntlProvider>);
+    const setRangeValuesFn = () => null;
+    const wrapper = mount(<IntlProvider locale="en">
+      <RangeSelector rangeValues={Map({ min: 0, max: 600 })} setRangeValues={setRangeValuesFn} />
+    </IntlProvider>);
 
     expect(wrapper.find(InputRange)).to.have.length(1);
   });
