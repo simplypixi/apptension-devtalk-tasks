f879d3fefcf98a95283cbf9627c8a1fc85f569ac
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2017-04-19 22:50:07 +0200

    test average in selectors

--- 7-testing-exercise/app/modules/teams/spec/teams.selectors.spec.js
+++ 7-testing-exercise/app/modules/teams/spec/teams.selectors.spec.js
@@ -1,4 +1,6 @@
 import { expect } from 'chai';
+import { fromJS } from 'immutable';
+import { times, constant } from 'lodash';
 
 import { filterTeamsListBySquadValue, selectArithmeticAverage } from '../teams.selectors';
 
@@ -6,4 +8,18 @@ describe('team selector', () => {
   it('should team selector be defined', () => {
     expect(filterTeamsListBySquadValue, selectArithmeticAverage).to.not.be.equal(undefined);
   });
+
+  describe('average', () => {
+    it('should proper count average', () => {
+      const fcOne = times(10, constant({name: 'FC One', squadMarketValue: '1,000,000 €'}));
+      const fcTwo = times(10, constant({name: 'FC Two', squadMarketValue: '2,000,000 €'}));
+
+      const state = fromJS({teams: {list: [...fcOne, ...fcTwo], rangeValues: {min: 0, max: 600}}});
+
+      expect(selectArithmeticAverage(state)).to.be.equal(1500000);
+    });
+  });
+
 });
+
+
