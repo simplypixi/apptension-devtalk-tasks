ab899679cc8a1066111e22e5065635c7c21395b4
MichalObi <ndwz123@gmail.com>
2017-04-20 16:36:33 +0200

    Mount test for languageSelector

--- 7-testing-exercise/app/routes/home/languageSelector/spec/languageSelector.component.spec.js
+++ 7-testing-exercise/app/routes/home/languageSelector/spec/languageSelector.component.spec.js
@@ -5,10 +5,12 @@ import LanguageSelector from '../languageSelector.component';
 
 describe('languageSelector component', () => {
   const wrapper = mount(<LanguageSelector value="en" />);
+  it('should mount a component', () => {
+    expect(wrapper.component.state.mount).to.be.true;
+  });
   it('should set up value as props', () => {
     expect(wrapper.props().value).to.equal('en');
   });
-
   it('should allow update value via props changing', () => {
     wrapper.setProps({ value: 'de' });
     expect(wrapper.props().value).to.equal('de');
