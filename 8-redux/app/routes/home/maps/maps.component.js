d5543f08d8398c1693dd42f8de8ae8b2b43f6950
Mariusz Wójcik <pixiproject@gmail.com>
2017-06-07 18:27:06 +0200

    Add marger image

--- 8-redux/app/routes/home/maps/maps.component.js
+++ 8-redux/app/routes/home/maps/maps.component.js
@@ -1,11 +1,21 @@
 import React, { PropTypes, PureComponent } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import L from 'leaflet';
+import DivIcon from 'react-leaflet-div-icon';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
 export class Maps extends PureComponent {
   static propTypes = {
     places: PropTypes.object,
+  };
+
+  getIcon = () => {
+    return new L.Icon({
+        iconUrl: 'https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/128/Map-Marker-Marker-Outside-Azure.png',
+        iconAnchor: new L.Point(24, 48),
+        iconSize: new L.Point(48, 48),
+    });
   };
 
   render() {
@@ -14,25 +24,29 @@ export class Maps extends PureComponent {
     const { lat, lon, display_name: displayName, icon, type } = place ? place.toJS() : { lat: '51.505', lon: '-0.09' };
     const position = [lat, lon];
     return (
-      <Card>
+      <Card className="map">
         <CardHeader
-          title={displayName}
+          title={displayName || 'There is no data...'}
           avatar={icon}
-          subtitle={`Type: ${type}`}
+          subtitle={type ? `Type: ${type}` : 'try with other query'}
         />
-        <section className="maps">
-          <Map center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-              attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-              url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
-            />
-            <Marker position={position}>
-              <Popup>
-                <span>{displayName}</span>
-              </Popup>
-            </Marker>
-          </Map>
-        </section>
+        {place ? (
+          <section className="map__container">
+            <Map center={position} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+                url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+              />
+              <Marker position={position}
+                      icon={this.getIcon()}
+              >
+                <Popup>
+                  <span>{displayName}</span>
+                </Popup>
+              </Marker>
+            </Map>
+          </section>
+        ) : ''}
       </Card>
     );
   }
