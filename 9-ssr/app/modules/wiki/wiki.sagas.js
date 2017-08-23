f0b63318f72f649f116871b95b81e3421298dbd3
MichalObi <ndwz123@gmail.com>
2017-08-23 19:35:13 +0200

    Cache wiki request

--- 9-ssr/app/modules/wiki/wiki.sagas.js
+++ 9-ssr/app/modules/wiki/wiki.sagas.js
@@ -1,7 +1,9 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { assign, toArray, pick } from 'lodash';
+import { assign, toArray, some } from 'lodash';
 import { parseJSON } from '../api/api.sagas';
 import { WikiTypes, WikiActions } from './wiki.redux';
+
+let cacheArray = [];
 
 export function* requestWikiSaga(url) {
   const headers = assign({
@@ -19,11 +21,23 @@ export function* requestWikiSaga(url) {
 
 export function* fetchWikiSaga({ wiki: { value, lang } }) {
   try {
-    const url = `https://${lang}.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&format=json&origin=*&titles=${value}`;
-    const data = yield call(requestWikiSaga, url);
-    const dataArray = toArray(data.query.pages);
-    const extractedData = dataArray[0];
-    yield put(WikiActions.fetchSuccess(extractedData));
+    if (cacheArray.length > 100) {
+      cacheArray = [];
+    }
+    if (cacheArray.length > 0 && some(cacheArray, { title: value })) {
+      const cachedValue = cacheArray.find((val) => val.title === value);
+      yield put(WikiActions.fetchSuccess(cachedValue));
+    } else {
+      const url = `https://${lang}.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&format=json&origin=*&titles=${value}`;
+      const data = yield call(requestWikiSaga, url);
+      const dataArray = toArray(data.query.pages);
+      const extractedData = dataArray[0];
+      if (extractedData.pageid && extractedData.extract !== '' && !some(cacheArray, extractedData)) {
+        cacheArray.push(extractedData);
+      }
+      yield put(WikiActions.fetchSuccess(extractedData));
+    }
+
   } catch (e) {
     console.log('api error', e);
     yield put(WikiActions.fetchError(e));
