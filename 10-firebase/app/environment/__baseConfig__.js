154da2039d2a0ee3aa1ef3552e96a75805357380
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2018-03-09 09:11:57 +0100

    Add boilerplate

---
+++ 10-firebase/app/environment/__baseConfig__.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  piKey: 'AIzaSyDc3rOGA5zZdlOK2wqrZdsEzEfSTMpwfEA',
+  authDomain: 'mikuchat-52e4c.firebaseapp.com',
+  databaseURL: 'https://mikuchat-52e4c.firebaseio.com',
+  projectId: 'mikuchat-52e4c',
+  storageBucket: 'mikuchat-52e4c.appspot.com',
+  messagingSenderId: '784391505489'
+};
+
+export default {
+  baseURL: '/',
+  firebaseConfig,
+};
