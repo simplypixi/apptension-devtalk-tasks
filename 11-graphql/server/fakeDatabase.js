edc1651d3d94a3d66c286d5a38179964c80af359
Tadeusz Lewandowski <tlewandowski@apptension.com>
2018-04-09 10:36:55 +0200

    Init basic graphql server

---
+++ 11-graphql/server/fakeDatabase.js
@@ -0,0 +1,42 @@
+const notes = [
+    {
+        id: "1",
+        content: 'My first note',
+        author_id: '0176413761b289e6d64c2c14a758c1c7'
+    },
+    {
+        id: "2",
+        content: 'Do something cool',
+        author_id: '2176413761b289e6d64ccccca758c1c4'
+    },
+    {
+        id: "3",
+        content: 'Do something',
+        author_id: '1176413761b289e6d64c2aaaa758c1c2'
+    },
+    {
+        id: "4",
+        content: 'Do something',
+        author_id: '0176413761b289e6d64c2c14a758c1c7'
+    }
+];
+
+const authors = [
+    {
+        id: '0176413761b289e6d64c2c14a758c1c7',
+        name: "John"
+    },
+    {
+        id: '1176413761b289e6d64c2aaaa758c1c2',
+        name: "George"
+    },
+    {
+        id: '2176413761b289e6d64ccccca758c1c4',
+        name: "Jan"
+    }
+];
+
+module.exports = {
+    notes,
+    authors
+}
