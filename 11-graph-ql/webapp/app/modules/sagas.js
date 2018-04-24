734d0223b6e821debf90480e05e5e5bcb4ea92c6
Paweł Mikutel <mikutel.pawel@gmail.com>
2018-04-25 00:01:21 +0200

    Add missing notes imports

--- 11-graph-ql/webapp/app/modules/sagas.js
+++ 11-graph-ql/webapp/app/modules/sagas.js
@@ -1,10 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 import maintainersSaga from './maintainers/maintainers.sagas';
+import { watchNotes } from './notes/notes.sagas';
 //<-- IMPORT MODULE SAGA -->
 
 export default function* rootSaga() {
   yield all([
     fork(maintainersSaga),
+    fork(watchNotes),
     //<-- INJECT MODULE SAGA -->
   ]);
 }
