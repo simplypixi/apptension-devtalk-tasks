82b23a6dae536be4ad739d520acf93f12e5d973d
Paweł Mikutel <mikutel.pawel@gmail.com>
2018-05-02 00:27:22 +0200

    Fix main server configuration

--- 11-graph-ql/server/index.js
+++ 11-graph-ql/server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { 
-    GraphQLSchema 
-} = require('graphql');
-
+const mongoose = require('mongoose');
+const {
+  GraphQLSchema
+  } = require('graphql');
+const Db = require('./db/Db');
+const mockDb = require('./db/mockDatabase');
 const {QueryType, Mutation} = require('./graphql/types');
+
+//mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://192.168.99.100:27017');
+Db.setDB(mongoose.connection);
+
+const db = Db.getDB();
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected!');
+
+  /* mocking */
+  mockDb.mock();
+});
 
 const appSchema = new GraphQLSchema({query: QueryType, mutation: Mutation});
 
@@ -12,8 +27,8 @@ const app = express();
 const cors = require('cors');
 
 app.use('/graphql', cors(), graphqlHTTP({
-    schema: appSchema,
-    graphiql: true,
+  schema: appSchema,
+  graphiql: true,
 }));
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
